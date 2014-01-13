11e811f6d8bf3a1e74a1f1658c05bc05c722c293
Lactem <lactem@outlook.com>
2014-01-12 23:49:20 -0800

    Add fields and getters/setters

--- src/main/java/com/stealthyone/mcb/gamegine/backend/arenas/Arena.java
+++ src/main/java/com/stealthyone/mcb/gamegine/backend/arenas/Arena.java
@@ -1,7 +1,72 @@
 package com.stealthyone.mcb.gamegine.backend.arenas;
 
+import java.util.ArrayList;
+import org.bukkit.Location;
+
+import com.stealthyone.mcb.gamegine.backend.selection.Selection;
+
 public class Arena {
+	private String name;
+	private Selection selection;
+	private ArrayList<String> players = new ArrayList<String>();
+	private Location spawn;
+	boolean pvp;
 
+	public Arena(String name, Selection selection) {
+		this.name = name;
+		this.selection = selection;
+	}
 
+	public String getName() {
+		return name;
+	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Selection getSelection() {
+		return selection;
+	}
+
+	public void setSelection(Selection selection) {
+		this.selection = selection;
+	}
+
+	public ArrayList<String> getPlayers() {
+		return players;
+	}
+
+	public void setPlayers(ArrayList<String> players) {
+		this.players = players;
+	}
+
+	public void addPlayer(String name) {
+		if (!players.contains(name)) {
+			players.add(name);
+		}
+	}
+
+	public void removePlayer(String name) {
+		if (players.contains(name)) {
+			players.remove(name);
+		}
+	}
+
+	public Location getSpawn() {
+		return spawn;
+	}
+
+	public void setSpawn(Location spawn) {
+		this.spawn = spawn;
+	}
+
+	public boolean isPvp() {
+		return pvp;
+	}
+
+	public void setPvp(boolean pvp) {
+		this.pvp = pvp;
+	}
+	
 }
