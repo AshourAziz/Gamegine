016b0459d280b7c16d1fdc1f0988098c68149d40
Stealth2800 <stealth2800@stealthyone.com>
2014-01-19 00:36:43 -0800

    Added loading/saving for arenas Added ability to define multiple spawns

--- src/main/java/com/stealthyone/mcb/gamegine/backend/arenas/Arena.java
+++ src/main/java/com/stealthyone/mcb/gamegine/backend/arenas/Arena.java
@@ -1,19 +1,39 @@
 package com.stealthyone.mcb.gamegine.backend.arenas;
 
 import com.stealthyone.mcb.gamegine.backend.selections.Selection;
+import com.stealthyone.mcb.stbukkitlib.lib.utils.LocationUtils;
 import org.bukkit.Location;
+import org.bukkit.configuration.ConfigurationSection;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Arena {
 
 	private String name;
 	private Selection selection;
-	private Location spawn;
-	private boolean pvp;
+	private List<Location> spawnPoints = new ArrayList<>();
+	private boolean pvp = false;
 
 	public Arena(String name, Selection selection) {
 		this.name = name;
 		this.selection = selection;
 	}
+
+    public Arena(ConfigurationSection config) {
+        this.name = config.getString("name");
+        this.pvp = config.getBoolean("pvp");
+        this.selection = new Selection(config.getConfigurationSection("selection"));
+        for (String rawLoc : config.getStringList("spawnPoints")) {
+            spawnPoints.add(LocationUtils.stringToLocation(rawLoc));
+        }
+    }
+
+    public void save(ConfigurationSection config) {
+        config.set("name", name);
+        selection.save(config.createSection("selection"));
+
+    }
 
 	public String getName() {
 		return name;
@@ -31,13 +51,21 @@ public class Arena {
 		this.selection = selection;
 	}
 
-	public Location getSpawn() {
-		return spawn;
+	public List<Location> getSpawn() {
+		return spawnPoints;
 	}
 
-	public void setSpawn(Location spawn) {
-		this.spawn = spawn;
-	}
+    public void addSpawn(Location location) {
+        spawnPoints.add(location);
+    }
+
+    public Location removeSpawn(int index) {
+        try {
+            return spawnPoints.remove(index);
+        } catch (IndexOutOfBoundsException ex) {
+            return null;
+        }
+    }
 
 	public boolean isPvp() {
 		return pvp;
