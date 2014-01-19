7092b59ed166a83be77997af4518371b6092400c
Stealth2800 <stealth2800@stealthyone.com>
2014-01-19 00:09:56 -0800

    Updated message layout and  configuration retrieving

--- src/main/java/com/stealthyone/mcb/gamegine/backend/selections/SelectionManager.java
+++ src/main/java/com/stealthyone/mcb/gamegine/backend/selections/SelectionManager.java
@@ -1,8 +1,7 @@
 package com.stealthyone.mcb.gamegine.backend.selections;
 
 import com.stealthyone.mcb.gamegine.Gamegine;
-import com.stealthyone.mcb.gamegine.config.ConfigBoolean;
-import com.stealthyone.mcb.gamegine.config.ConfigString;
+import com.stealthyone.mcb.gamegine.config.ConfigHelper;
 import com.stealthyone.mcb.stbukkitlib.lib.utils.MaterialUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -29,21 +28,21 @@ public class SelectionManager {
         /* Check config values */
         log.log(Level.INFO, "");
         log.log(Level.INFO, "-----Gamegine Configuration: Selections-----");
-        if (ConfigBoolean.SELECTIONS_USE_WORLDEDIT.get()) {
+        if (ConfigHelper.SELECTIONS_USE_WORLDEDIT.get()) {
             //Check dependencies
             log.log(Level.INFO, "Using WorldEdit for selection wand.");
         } else {
             log.log(Level.INFO, "Using built-in wand for selections.");
         }
 
-        String rawWand = ConfigString.SELECTIONS_WAND.get();
+        String rawWand = ConfigHelper.SELECTIONS_WAND.get();
         if (rawWand == null) {
-            log.log(Level.INFO, "No wand specified in config, wand disabled.");
+            log.log(Level.INFO, "No wand specified in config, wand DISABLED.");
         } else {
             try {
                 selectionWand = MaterialUtils.rawMaterialToItem(rawWand, 1);
             } catch (IllegalArgumentException ex) {
-                log.log(Level.INFO, "ERROR: Error loading wand type from config. (" + ex.getMessage() + ") Wand disabled.");
+                log.log(Level.INFO, "ERROR: Error loading wand type from config. (" + ex.getMessage() + ") Wand DISABLED.");
             }
         }
     }
