60f6e36ade721a614073a9e1c9c4ba95a5300808
Stealth2800 <stealth2800@stealthyone.com>
2014-04-16 11:20:48 -0700

    Massive changes to reflect new API changes. No longer 'GamegineCore', the project is now separated into 'Gamegine' and 'GamegineAPI'.

---
+++ src/main/java/com/stealthyone/mcb/gamegine/GameginePlugin.java
@@ -0,0 +1,22 @@
+package com.stealthyone.mcb.gamegine;
+
+import com.stealthyone.mcb.stbukkitlib.lib.plugins.StPlugin;
+
+public class GameginePlugin extends StPlugin {
+
+    @Override
+    protected void handleLoad() {
+
+    }
+
+    @Override
+    protected void handleEnable() {
+
+    }
+
+    @Override
+    protected void handleDisable() {
+
+    }
+
+}
--- src/main/java/com/stealthyone/mcb/gameginecore/backend/points/PointManager.java
+++
@@ -1,7 +0,0 @@
-package com.stealthyone.mcb.gameginecore.backend.points;
-
-public class PointManager {
-
-
-
-}
